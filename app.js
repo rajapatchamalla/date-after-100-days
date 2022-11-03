f8a608130ac8d6d1d994debdf86b103ed74779c0
root <patchamallaraja1996@gmail.com>
2022-11-03 06:46:16 +0000

    creating date

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const addDays = require("date-fns/addDays");
+const app = express();
+
+app.get("/", (request, response) => {
+  const result = addDays(new Date(), 100);
+  response.send(
+    `${result.getDate()}/${result.getMonth()}/${result.getFullYear()}`
+  );
+});
+module.exports = app;
